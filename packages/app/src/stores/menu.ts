3e30add46d34c16f17ca560fb4e4c4b5cd62697d
huaiyuan <1029848564@qq.com>
2024-05-16 11:57:25 +0800

    fixbug: Files chunk upload

--- packages/app/src/stores/menu.ts
+++ packages/app/src/stores/menu.ts
@@ -202,7 +202,7 @@ export const useMenuStore = defineStore('menu', {
 					{
 						label: VaultMenuItem.LOCKSCREEN,
 						key: VaultMenuItem.LOCKSCREEN,
-						icon: 'sym_r_casino'
+						icon: 'sym_r_lock'
 					},
 					{
 						label: VaultMenuItem.SETTINGS,
@@ -266,6 +266,16 @@ export const useMenuStore = defineStore('menu', {
 				const elChildren: any = teamsMenus.children[0];
 				elChildren.label = orgs[0].name;
 				elChildren.key = orgs[0].name;
+
+				console.log('--------TEST INVITES START--------');
+				console.log('orgs', orgs);
+				console.log('orgs[0]', orgs[0]);
+				console.log('orgs[0].owner', orgs[0].owner);
+				console.log('app', app);
+				console.log('app.account', app.account);
+				console.log('app.account?.did', app.account?.did);
+				console.log('elChildren.children', elChildren.children);
+				console.log('--------TEST INVITES END--------');
 
 				if (orgs[0].owner.did !== app.account?.did) {
 					elChildren.children = elChildren.children.filter(
