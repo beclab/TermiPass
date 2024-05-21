bf99e6df41247814a2daf50b6daa5a936e88e8b5
qq815776412 <815776412@qq.com>
2024-05-21 11:05:06 +0800

    feat: update build version

--- packages/app/build/updateVersion.js
+++ packages/app/build/updateVersion.js
@@ -1,19 +1,13 @@
 const path = require('path');
 const fs = require('fs');
-const { execSync } = require('child_process');
 
 console.log('NODE_ENV:', process.env.TAG_VERSION);
 const regex = /^v(\d+)\.(\d+)\.(\d+)$/;
 const match = process.env.TAG_VERSION.match(regex);
 if (!match) {
-	console.log('TAG_VERSION 不符合要求');
 	return;
 }
-console.log('TAG_VERSION 符合要求');
 const [, major, minor, patch] = match;
-console.log('Major:', major);
-console.log('Minor:', minor);
-console.log('Patch:', patch);
 
 try {
 	const appPackageJsonUrl = path.join(process.cwd(), 'package.json');
@@ -21,34 +15,6 @@ try {
 	const appPackage = JSON.parse(appPackageJson);
 	appPackage.version = `${major}.${minor}.${patch}`;
 	fs.writeFileSync(appPackageJsonUrl, JSON.stringify(appPackage, null, 2));
-	// 执行 git tag 命令，获取所有标签，并将结果转换成字符串
-	const tagsOutput = execSync('git tag').toString();
-
-	// 将字符串按换行符分割成数组，并获取数组的长度，即标签数量
-	const numberOfTags = tagsOutput.trim().split('\n').length + 10;
-
-	const capacitorPackageJsonUrl = path.join(
-		process.cwd(),
-		'src-capacitor/package.json'
-	);
-	const capacitorPackageJson = fs.readFileSync(
-		capacitorPackageJsonUrl,
-		'utf-8'
-	);
-	const capacitorPackage = JSON.parse(capacitorPackageJson);
-	capacitorPackage.versionCode = `${numberOfTags}`;
-	fs.writeFileSync(
-		capacitorPackageJsonUrl,
-		JSON.stringify(capacitorPackage, null, 2)
-	);
-
-	console.log('标签数量:', numberOfTags);
 } catch (error) {
 	console.log(error);
 }
-
-// try {
-
-// } catch (error) {
-// 	console.error('获取标签数量时出错:', error.message);
-// }
