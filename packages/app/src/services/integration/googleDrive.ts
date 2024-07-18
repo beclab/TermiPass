e958bab2ca0e59546a90d1bfcc685961b1cc8e5b
icebergtsn <zyh2433219116@gmail.com>
2024-07-18 16:38:00 +0800

    feat: update android refresh token

--- packages/app/src/services/integration/googleDrive.ts
+++ packages/app/src/services/integration/googleDrive.ts
@@ -5,6 +5,8 @@ import {
 	GoogleIntegrationAccount,
 	OperateIntegrationAuth
 } from '../abstractions/integration/integrationService';
+import { getAppPlatform } from '../../platform/appPlatform';
+import { axiosInstanceProxy } from '../../platform/httpProxy';
 
 export class GoogleAuthService extends OperateIntegrationAuth<GoogleIntegrationAccount> {
 	type = AccountType.Google;
@@ -16,13 +18,41 @@ export class GoogleAuthService extends OperateIntegrationAuth<GoogleIntegrationA
 		});
 		await GoogleAuth.signOut();
 		const googleDriveSignInResponse = await GoogleAuth.signIn();
+
+		let response;
+
+		if (getAppPlatform().getQuasar()?.platform.is?.android) {
+			const form = new FormData();
+			form.append('code', googleDriveSignInResponse.serverAuthCode);
+			response = await axiosInstanceProxy({
+				baseURL: 'https://cloud-dev-api.jointerminus.com',
+				timeout: 10000,
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			}).post('/v1/common/google/token', {
+				code: googleDriveSignInResponse.serverAuthCode
+			});
+			console.log(response);
+			if (!response || response.data.code !== 200 || !response.data.data) {
+				throw new Error(
+					'Exchange authorization code error ' + response.data.data
+						? response.data.data.message
+						: ''
+				);
+			}
+		}
+
 		return {
 			name: googleDriveSignInResponse.email,
 			type: AccountType.Google,
 			raw_data: {
-				access_token: googleDriveSignInResponse.authentication.accessToken,
-				refresh_token:
-					googleDriveSignInResponse.authentication.refreshToken || '',
+				access_token: response
+					? response.data.data.accessToken
+					: googleDriveSignInResponse.authentication.accessToken,
+				refresh_token: response
+					? response.data.data.refreshToken
+					: googleDriveSignInResponse.authentication.refreshToken || '',
 				expires_at: Date.now() + 30 * 60 * 1000,
 				expires_in: 30 * 60 * 1000,
 				scope: scopes.join(','),
