99a4870d12e7d849b7fa6561de1b110d795c8ee4
qq815776412 <815776412@qq.com>
2024-07-23 19:11:48 +0800

    fix: macOS files sync bug

--- packages/app/build/electron/afterSign.js
+++ packages/app/build/electron/afterSign.js
@@ -1,19 +1,19 @@
-// const { notarize } = require('@electron/notarize');
+const { notarize } = require('@electron/notarize');
 const afterSign = async (context) => {
-	// const { electronPlatformName, appOutDir } = context;
-	// if (electronPlatformName !== 'darwin') {
-	// 	return;
-	// }
-	// const macosConfig = require('../mac/notarize');
-	// console.log('macos notarize');
-	// const appName = context.packager.appInfo.productFilename;
-	// return await notarize({
-	// 	appBundleId: 'com.terminus.planetam',
-	// 	appPath: `${appOutDir}/${appName}.app`,
-	// 	appleId: macosConfig.appleId,
-	// 	appleIdPassword: macosConfig.appleIdPassword,
-	// 	tool: 'notarytool',
-	// 	teamId: macosConfig.teamId
-	// });
+	const { electronPlatformName, appOutDir } = context;
+	if (electronPlatformName !== 'darwin') {
+		return;
+	}
+	const macosConfig = require('../mac/notarize');
+	console.log('macos notarize');
+	const appName = context.packager.appInfo.productFilename;
+	return await notarize({
+		appBundleId: 'com.terminus.planetam',
+		appPath: `${appOutDir}/${appName}.app`,
+		appleId: macosConfig.appleId,
+		appleIdPassword: macosConfig.appleIdPassword,
+		tool: 'notarytool',
+		teamId: macosConfig.teamId
+	});
 };
 module.exports = afterSign;
