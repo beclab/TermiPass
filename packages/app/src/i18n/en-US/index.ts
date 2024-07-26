d1196577e7036163a2e46cf1720b5e7fc5fc2664
qq815776412 <815776412@qq.com>
2024-07-26 18:06:13 +0800

    feat: fix mobile video error

--- packages/app/src/i18n/en-US/index.ts
+++ packages/app/src/i18n/en-US/index.ts
@@ -1155,7 +1155,7 @@ If you want to create an organization, please refer to the steps listed in the T
 	domain_list: 'Domain List',
 	refresh: 'refresh',
 	select_binding_rules: 'Select binding rules',
-	fixed_email_suffix: 'Fixed Email_Suffix',
+	fixed_email_suffix: 'Fixed email Fuffix',
 	please_enter_your_organization_email_suffix_enter_multiple_separated_by_commas:
 		"Please enter your organization's email suffix, Enter multiple, separated by commas.",
 
